28946637054962e86578886c39e5556b075ee604
Lissa Aguilar <lissa.aguilar@gmail.com>
2011-02-10 21:34:36 -0500

    added new paypal button to corseted t

--- corset-boy.php
+++ corset-boy.php
@@ -6,15 +6,26 @@
 										<p style="margin: 5px 0 0 34px;">S, M, L<br />$28.00<br /></p>
 						<div style="margin:-27px 0 0 100px;">
 							<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-												<input name="cmd" value="_xclick" type="hidden">
-												<input name="business" value="jj.sweetgwendoline@gmail.com" type="hidden">
-												<input name="item_name" value="Butterfly Corset lace-up back tank" type="hidden">
-												<input name="amount" value="48.00" type="hidden">
-												<input name="no_note" value="1" type="hidden">
-												<input name="currency_code" value="USD" type="hidden">
-												<input class="buybtn" name="submit" value="" title="Buy now with PayPal" type="submit">
-												<input name="add" value="1" type="hidden">
-											</form>
+								<input type="hidden" name="cmd" value="_s-xclick">
+								<input type="hidden" name="hosted_button_id" value="GMBSMLV8768LU">
+							<table>
+								<tr><td><input type="hidden" name="on0" value="Size">Size</td></tr><tr><td>
+								<select name="os0">
+									<option value="Small">Small </option>
+									<option value="Medium">Medium </option>
+									<option value="Large">Large </option>
+								</select> </td></tr>
+							<tr><td><input type="hidden" name="on1" value="Color">Color</td></tr><tr><td>
+									<select name="os1">
+										<option value="White">White </option>
+										<option value="Black">Black </option>
+										<option value="Option 3">Option 3 </option>
+									</select> </td></tr>
+							</table>
+								<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+								<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							</form>
+						
 						</div>
 						<p style="margin:55px 0 0 34px; position:absolute;width:100px;">Front</p>
 						<p style="margin:55px 34px 0 0; float:right; position:absolute;width:100px;">Back</p>
