e82acd72df66bfb4fad8f2d39f497e37d5a89634
Lissa Aguilar <lissa.aguilar@gmail.com>
2011-02-13 21:12:26 -0500

    removed updates info from page

--- updates.php
+++ updates.php
@@ -1,9 +1,7 @@
 ﻿<?php require_once 'header.php'; ?>
 
 	<img src="images/updates_header.jpg" width="754" alt="Sweet Gwendoline Updates" />
-	<div style="margin-top:30px;">						
 <?php
-
 ?>	
 
 </div>
