72e59c35db1faf57040245d3adcf00d61eef4915
Lissa Aguilar <lissa.aguilar@gmail.com>
2011-02-28 15:36:13 -0500

    used jquery to place mouseover for take a peek

--- collection.php
+++ collection.php
@@ -2,11 +2,10 @@
 <script type="text/javascript">
 			$(document).ready(function(){
 				$('.view').hide();
-				$('.cover.boxcaption').hide();
-				$('.product captionfull').mouseover(function(){
-					$('.cover').show();
+				$('.product').mouseover(function(){
+					$(this).find('.view').show();
 				}).mouseout(function(){
-					$('.cover').hide();
+					$(this).find('.view').hide();
 				});
 	/*	$('.product.captionfull').hover(function(){
 				$('.cover', this).stop().animate({top:'130px'}, {queue:false, duration:160});
@@ -14,9 +13,9 @@
 				$('.cover', this).stop().animate({top:'230px'}, {queue:false, duration:160});
 		});*/
 			$('.product-last').mouseover(function(){
-				$('.view').show();
+				$(this).find('.view').show();
 				}).mouseout(function(){
-					$('.view').hide();
+					$(this).find('.view').hide();
 				});
 	});
 		</script>
@@ -24,85 +23,88 @@
 		<div id="collection">
 					<img src="images/collections_header.jpg" alt=" Sweet Gwendoline Clothing Collection" />
 					<br />
-							<p style="margin-left:550px">Scroll Down to See More</p>
+							<p style="margin-top:10px;margin-left:600px;font-size:.8em">Scroll Down to See More</p>
 							<div id="product_container" style="margin-top:5px">
 										
-							<div class="product captionfull"><a href="waspwaist.php">
-												<img src="images/wasp-waist-karen.jpg" width=177px" /></a>
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-														<div class="details"><p>Wasp Waist<br />$28.00<br /></p></div>
+							<div class="product"><a href="waspwaist.php">
+												<img src="images/wasp-waist-karen.jpg" width="177px" /><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+														<div class="details" style="margin-top:230px;position:absolute;"><p>Wasp Waist<br />$28.00<br /></p></div>
 							</div>
 
-							<div class="product captionfull">
-										<a href="butterfly-heels.php"><img src="images/butterfly-in-heels-karen.jpg" style="width:177px;" /></a>
-										<div class="cover boxcaption"><p>take a peek</p></div>	
-											<p class="details">Butterfly in Heels<br />$33.00</p>
+							<div class="product ">
+										<a href="butterfly-heels.php"><img src="images/butterfly-in-heels-karen.jpg" style="width:177px;" /><div class="view" style="position:absolute;margin-top :120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+											<p class="details"style="margin-top:230px;position:absolute;">Butterfly in Heels<br />$33.00</p>
 							</div>
 
-							<div class="product captionfull"><a href="about-this-tank.php">
-											<img src="images/about-these-corsets-tank.jpg" style="width:177px;" /></a>
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-											<p class="details">About These Corsets<br />$40.00<br /></p>
+							<div class="product"><a href="about-this-tank.php">
+											<img src="images/about-these-corsets-tank.jpg" style="width:177px;" /><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+											<p class="details"style="margin-top:230px;position:absolute;">About These Corsets<br />$40.00<br /></p>
 							</div>
 								
 							<div class="product-last"><a href="star-bizarre.php">
-												<img src="images/star-bizarre-margie.jpg" style="width:177px;" /></a>
-												<p class="details">Star Bizarre<br />$28.00<br /></p>
+												<img src="images/star-bizarre-margie.jpg" style="width:177px;" /><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+												<p class="details"style="margin-top:230px;position:absolute;">Star Bizarre<br />$28.00<br /></p>
 							</div>
 								
 							<div class="product captionfull"><a href="crawl.php">
-												<img src="images/crawl-margie.jpg" style="width:177px;" /></a
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-												<p class="details">Crawl<br />$28.00<br /></p>
+												<img src="images/crawl-margie.jpg" style="width:177px;" /><div class="view" style="position:absolute;margin-top :120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+												<p class="details"style="margin-top:230px;position:absolute;">Crawl<br />$28.00<br /></p>
 							</div>
 								
 							<div class="product captionfull"><a href="pretty-polly.php">
-													<img src="images/pretty-polly-karen.jpg" alt="Pretty Polly" style="width:177px;" /></a>
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-													<p class="details">Pretty Polly<br />$28.00<br /></p>
+													<img src="images/pretty-polly-karen.jpg" alt="Pretty Polly" style="width:177px;" /><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+													<p class="details"style="margin-top:230px;position:absolute;">Pretty Polly<br />$28.00<br /></p>
 							</div>
 								
 							<div class="product captionfull"><a href="tamed.php">
-											<img src="images/tamed-karen.jpg" style="width:177px;" /></a>
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-											<p class="details">Tamed<br />$33.00<br /></p>
+											<img src="images/tamed-karen.jpg" style="width:177px;" /><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+											<p class="details"style="margin-top:230px;position:absolute;">Tamed<br />$33.00<br /></p>
 							</div>
 								
 							<div class="product-last"><a href="plead.php">
-											<img src="images/plead-karen.jpg" style="width:177px;" /></a>
-											<p class="details">Plead<br />$28.00<br /></p>
+											<img src="images/plead-karen.jpg" style="width:177px;" /><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+											<p class="details"style="margin-top:230px;position:absolute;">Plead<br />$28.00<br /></p>
 								
 							</div>
 								
 							<div class="product captionfull"><a href="mistress.php">
-											<img src="images/mistress-karen.jpg" style="width:177px;" /></a>
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-											<p class="details">Mistress<br />$33.00<br /></p>
+											<img src="images/mistress-karen.jpg" style="width:177px;" /><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+											<p class="details"style="margin-top:230px;position:absolute;">Mistress<br />$33.00<br /></p>
 							</div>
 
 								
 							<div class="product captionfull"><a href="corset-boy.php">
-											<img src="images/corset-boy-beater.jpg" style="width:177px;" /></a>
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-											<p class="details">Corseted Boy Beater<br />$48.00<br /></p>
+											<img src="images/corset-boy-beater.jpg" style="width:177px;" />
+											<div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+
+											<p class="details"style="margin-top:230px;position:absolute;">Corseted Boy Beater<br />$48.00<br /></p>
 							</div>
 
 							<div class="product captionfull"><a href="mesh-lace.php">
-											<img src="images/mesh-lace-boy-beater.jpg" style="width:177px;" /></a>
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-											<p class="details">Plead Mesh & Lace Boy Beater<br />$48.00<br /></p>
+											<img src="images/mesh-lace-boy-beater.jpg" style="width:177px;" />
+												<div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+											<p class="details"style="margin-top:230px;position:absolute;">Plead Mesh & Lace Boy Beater<br />$48.00<br /></p>
 							</div>
 								
 							<div class="product-last"><a href="wench-boy.php">
 											<img src="images/wench-boy-karen.jpg" style="width:177px;" /></a>
-											<div class="view" style="position:absolute;color:blue;margin-top :-120px;">View</div>
-											<p class="details">Wench Boy Beater<br />$48.00<br /></p>
+											<a href="wench-boy.php"><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+											<p class="details"style="margin-top:230px;position:absolute;">Wench Boy Beater<br />$48.00<br /></p>
 							</div>
 
 							<div class="product captionfull"><a href="teacup.php">
 												<img src="images/teacup.jpg" alt="Sweet Gwendoline Tea Cup" style="width:177px;" /></a>
-												<div class="cover boxcaption"><p>Take a Peek</p></div>
-												<p class="details">Tied Up for Tea<br />$20.00<br /></p>
+												<a href="teacup.php"><div class="view" style="position:absolute;margin-top:120px;margin-left:9px;height:25px;width:159px;background:url(images/sg_buttons.png) no-repeat 0 -170px"></div></a>
+												<p class="details"style="margin-top:230px;position:absolute;">Tied Up for Tea<br />$20.00<br /></p>
 						
 							</div>
 							<div style="clear:both"><br /></div>
